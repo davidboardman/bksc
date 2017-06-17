ec9e5ac12f3f05209c1c4bd523fc7ac4ee8719bc
David Boardman <dboardman@ideo.com>
2017-06-17 19:40:18 -0400

    done

--- js/bksc.js
+++ js/bksc.js
@@ -4,3 +4,18 @@ $('[href^="#"]').off().on('click', function (e) {
         $('body').animate({scrollTop: $($(this).attr('href')).offset().top}, 500);
     }
 });
+
+
+$(document).ready(function() {
+    $('.single-item').slick({
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+		autoplay: true,
+        autoplaySpeed: 5000,
+        arrows: true,
+    });
+});
+
